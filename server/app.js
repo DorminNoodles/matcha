0737e9ade05e0b8dd8341c857261d7332c158d5c
Loic CHETY <lchety@e2r1p8.42.fr>
2018-11-28 16:12:34 +0100

    wip

--- server/app.js
+++ server/app.js
@@ -9,14 +9,6 @@ const app = express();
 
 console.log(emitter.hello);
 
-emitter.hello = 9;
-// let test = new activationMail();
-
-// emitter.on('userRegistered', activationMail.sendActivationMail);
-
-
-// activationMail.displayMessage();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
