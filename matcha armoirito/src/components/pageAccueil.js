33f65d41520a145721f261fae093818ae08286df
Pascal Nguyen <van-sang@hotmail.fr>
2018-12-05 15:03:46 +0100

    debut de front pour lolo

---
+++ matcha armoirito/src/components/pageAccueil.js
@@ -0,0 +1,20 @@
+// React
+import React, {Fragment} from 'react'
+import {Link} from 'react-router-dom'
+
+//Style
+import '../style/pageAccueil.css'
+
+const PageAccueil = () => {
+
+    return (
+        <Fragment>
+            <div className="description">
+                <h1 className="title">Matcha</h1>
+                <Link to="/inscription"><p><button  className="btn btn-info btn-lg">Inscription</button></p></Link>
+            </div>
+        </Fragment>
+    )
+}
+
+export default PageAccueil
