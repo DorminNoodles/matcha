6443c0621f039e1046ea666ad357d792be1e0420
Pascal Nguyen <van-sang@hotmail.fr>
2018-12-05 14:53:19 +0100

    debut de front pour lolo

---
+++ matcha pascalito2/src/index.js
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+import ReactDom from 'react-dom';
+
+//Component
+import App from './containers/app';
+
+//Style
+import './style/index.css';
+
+
+
+ReactDom.render(<App />, document.querySelector('#root'));
