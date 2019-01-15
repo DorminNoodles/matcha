79eab0f02be782a17ce68e62701660a123d215cf
Loic CHETY <lchety@e2r3p6.42.fr>
2019-01-15 15:37:03 +0100

    findLocationByIp

--- server/services/geoloc.js
+++ server/services/geoloc.js
@@ -1,20 +1,16 @@
 "use strict";
-
-const iplocation = require("iplocation").default;
-
+const axios = require('axios');
+const geoip = require("geoip-lite");
 
 exports.findLocationByIp = () => {
 	return new Promise((resolve, reject) => {
-		
+		axios.get('https://api.ipify.org/?format=json')
+		.then((response) => {
+			console.log(geoip.lookup(response.data.ip));
+			resolve(geoip.lookup(response.data.ip));
+		})
+		.catch((err) => {
+			reject(err);
+		})
 	})
 }
-// class Geoloc {
-//
-// 	constructor(){
-//
-//
-// 	}
-//
-//
-//
-// }
