446decbed7b3f69a8e463b28d917fcebdbab8187
Lisa TRAN <ltran@e2r12p14.42.fr>
2019-05-20 19:36:21 +0200

    ++

--- my-app/src/component/Field.js
+++ my-app/src/component/Field.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-function Field(props) {
-    let { position, icon, onChange, placeholder } = { ...props }
-    
+function Field({ position, icon, onChange, placeholder }) {
+
     return (
         <div className="field">
 
             <p className={`control has-icons-${position}`}>
-                <input className="input" type="text" placeholder={placeholder} onChange={onChange}/>
+                <input className="input" type="text" placeholder={placeholder} onChange={onChange} />
                 <span className={`icon is-small is-${position}`}>
                     <i className={icon}></i>
                 </span>
