1cb180211205fbd67a5e01b28449ef3f1bb1e02d
Lisa TRAN <ltran@e3r9p10.42.fr>
2019-06-04 17:40:34 +0200

    Add(header/side change color)

--- my-app/src/container/Routers.js
+++ my-app/src/container/Routers.js
@@ -3,59 +3,69 @@ import { Home, User, Match, Chat, Parameters, Signin, Signup, Password } from '.
 import { Test } from './Test'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-
-
-
-
 class Routers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { className: "white-red" }
+    this.onClick = this.onClick.bind(this)
+  }
+
+  onClick = (i) => {
+    let { className } = this.state
+
+    if (className === "white-red" && i === 1) 
+      this.setState({ className: "red-white" })
+    else if (className === "red-white" && i === 0)
+      this.setState({ className: "white-red" })
+  }
 
   logout = (props) => {
     console.log(props)
-  
+
     // this.props.history.push('/')
   }
+
   render() {
+    let { className } = this.state
 
     return (
       <Router>
 
-        <nav id="side">
+        <nav id="side" className={className}>
           <ul>
             <li>
-              <Link to="/"><i className="fas fa-home fa-lg header-link"></i></Link>
+              <Link to="/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-home fa-lg header-link"></i></Link>
             </li>
             <li>
-              <Link to="/user/"><i className="fas fa-user fa-lg header-link"></i></Link>
+              <Link to="/user/" className={className} onClick={() => this.onClick(1)}><i className="fas fa-user fa-lg header-link"></i></Link>
             </li>
             <li>
-              <Link to="/match/"><i className="fas fa-heart fa-lg header-link"></i></Link>
+              <Link to="/match/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-heart fa-lg header-link"></i></Link>
             </li>
             <li>
-              <Link to="/chat/"><i className="fas fa-comments fa-lg header-link"></i></Link>
+              <Link to="/chat/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-comments fa-lg header-link"></i></Link>
             </li>
             <li>
-              <Link to="/signin/"><i className="fas fa-sign-in-alt fa-lg header-link"></i></Link>
+              <Link to="/signin/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-sign-in-alt fa-lg header-link"></i></Link>
             </li>
             <li>
 
-
-              <a href="#" onClick={ () => this.logout(this)}><i className="fas fa-sign-out-alt fa-lg header-link"></i></a>
+              <a href="#" onClick={() => this.logout(this)}><i className="fas fa-sign-out-alt fa-lg header-link"></i></a>
 
             </li>
             <li>
-              <Link to="/signup/"><i className="fas fa-user-plus"></i></Link>
+              <Link to="/signup/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-user-plus"></i></Link>
             </li>
             <li>
-              <Link to="/test/"><i className="fas fa-lemon"></i></Link>
+              <Link to="/test/" className={className} onClick={() => this.onClick(0)}><i className="fas fa-lemon"></i></Link>
             </li>
           </ul>
         </nav>
 
-        <nav id="header">
+        <nav id="header" className={className}>
           <ul>
             <li>
-              <Link to="/parameters"><i className="fas fa-cog fa-lg"></i></Link>
+              <Link to="/parameters" className={className} onClick={() => this.onClick(0)}><i className="fas fa-cog fa-lg"></i></Link>
             </li>
           </ul>
         </nav>
