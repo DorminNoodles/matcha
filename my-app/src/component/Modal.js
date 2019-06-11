4d88bf75cf80897a759863ae31ed1d0027d5b4a5
Lisa TRAN <ltran@e3r12p14.42.fr>
2019-06-11 18:57:28 +0200

    inProgress(Password component)

--- my-app/src/component/Modal.js
+++ my-app/src/component/Modal.js
@@ -5,18 +5,18 @@ class Modal extends React.Component {
     render() {
         return (
 
-            <div class="modal ">
-                <div class="modal-background"></div>
-                <div class="modal-content" style={{ alignItems: "center", display: "flex", justifyContent: "center" }}>
+            <div className="modal ">
+                <div className="modal-background"></div>
+                <div className="modal-content" style={{ alignItems: "center", display: "flex", justifyContent: "center" }}>
                     <span>
-                        <i class="fas fa-chevron-left"></i>
+                        <i className="fas fa-chevron-left"></i>
                     </span>
                     <img className="is-square" style={{ width: "400px", height: "400px", display: "flex", margin: "auto" }} alt="username" src="https://i.pinimg.com/originals/ba/de/f7/badef7c18045efffdab154a0b968a158.jpg"  />
                     <span>
-                        <i class="fas fa-chevron-right"></i>
+                        <i className="fas fa-chevron-right"></i>
                     </span>
                 </div>
-                <button class="modal-close is-large" aria-label="close"></button>
+                <button className="modal-close is-large" aria-label="close"></button>
             </div>
         )
     }
--- my-app/src/container/Password.js
+++ my-app/src/container/Password.js
@@ -4,19 +4,44 @@ import { Field } from "../export"
 class Password extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        }
+        this.state = { value: "", error: "" }
+        this.forgot = this.forgot.bind(this)
     }
+
+    onChange = (e) => {
+        this.setState({
+            ...this.state,
+            value: e.target.value
+        })
+    }
+
+    forgot = (e) => {
+        console.log(e.target.value)
+        // if (typeof rsl === 'object')
+        //     this.setState(rsl)
+        // else {
+
+        //     axios({
+        //         method: 'post',
+        //         url: 'http://localhost:3300/api/user/forgot',
+        //         data,
+        //         config: { headers: { 'Content-Type': 'multipart/form-data' } }
+        //     }).then(response => {
+        //         console.log(response)
+        //     }).catch(error => {
+        //         console.log({ ...error })
+        //         console.log(error.response.data.msg)
+        //     });
+        // }
+    }
+
     render() {
         return (
             <div id="signin">
-
                 <p style={{ fontFamily: "LadylikeBB", fontSize: "xx-large" }}>Matcha</p>
                 <br></br>
-                <Field placeholder="Email" position="left" icon="fas fa-envelope" />
-                <button className="button white-red" onClick={(e) => { this.register(e) }} >Send</button>
-
+                <Field placeholder="Email" position="left" icon="fas fa-envelope" onChange={this.onChange} />
+                <button className="button white-red" onClick={(e) => { this.forgot(e) }} >Send</button>
             </div>
         );
     }
