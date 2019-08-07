dfad6ab0a7ec9669202fbcbc3f9a1631718276b7
Loic CHETY <lchety@e1r7p8.42.fr>
2019-08-07 16:32:08 +0200

    wip

---
+++ server/services/geolocation.js
@@ -0,0 +1,23 @@
+const emitter = require('../emitter.js');
+const db = require('../controllers/database');
+
+
+class Geolocation {
+
+	constructor() {
+
+		emitter.on('userRegistered', this.checkLocationInDb);
+
+	}
+
+	checkLocationInDb(data) {
+
+
+		console.log('Geolocation Class >> ', data);
+
+	}
+
+
+}
+
+module.exports = new Geolocation();
