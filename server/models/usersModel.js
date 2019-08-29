7beb4ad3c17e71dc16b0c1f4632f2dd2e8897142
Lisa TRAN <ltran@e1r8p9.42.fr>
2019-08-29 16:27:23 +0200

    Add(queryUsers tags, age,score)

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -65,27 +65,43 @@ const database = require('../controllers/database');
 //    INNER JOIN tags ON usertags.tag_id=tags.id \
 //    WHERE tags.tag="feu")', arg);
 
+queryTags = (tags, arg) => {
+
+	let query = " id IN (SELECT DISTINCT usertags.user_id FROM usertags INNER JOIN tags ON usertags.tag_id=tags.id WHERE "
+
+	for (i = 0; i < tags.length; i++) {
+		query += 'tags.tag=?'
+		query += (i < tags.length - 1) ? " || " : ")"
+	}
+
+	arg = arg.concat(tags)
+
+	return ({ query, arg })
+
+}
+
 getQuery = ({ age_min, age_max, distance, score, tags }) => {
 
-	let s_score = " WHERE compatibility >= ?"
-	let s_age = "age BETWEEN ? AND ?"
-	let s_tag = "id IN \
-				(SELECT DISTINCT usertags.user_id \
-					FROM usertags \
-					INNER JOIN tags ON usertags.tag_id=tags.id \
-					WHERE tags.tag='feu')"
-	console.log("coucou")
-
+	let s_score = " compatibility >= ? "
+	let s_age = " age BETWEEN ? AND ? "
 
 	let query = ""
-	let arg = ""
-	if (tags && tags.length > 0)
-		query = s_tag
-	if (age_min && age_max)
-		query = query.length > 0 ? s_age + " && " + query : s_age
-	if (score)
-		query = query.length > 1 ? s_score + " && " + query : s_score
-	return { query }
+	let arg = []
+
+	if (score && arg.push(score)) { query += s_score }
+
+	if (age_min && age_max && arg.push(age_min, age_max))
+		query += query.length > 0 ? "&&" + s_age : s_age
+
+	if (tags && tags.length > 0) {
+		r_tags = queryTags(tags, arg)
+		arg = r_tags.arg
+		query += query.length > 0 ? "&&" + r_tags.query : r_tags.query
+	}
+
+	if (query.length > 0) { query = " WHERE" + query }
+
+	return { query, arg }
 }
 
 
@@ -93,26 +109,15 @@ exports.get = (params) => {
 	return new Promise((resolve, reject) => {
 		database.connection()
 			.then((conn) => {
-				console.log("params", params)
+				rsl = getQuery(params)
 
-				let arg = [];
-				query = getQuery(params)
-
-				return conn.query(
-					'SELECT * FROM users' + query, arg);
+				return conn.query('SELECT * FROM users' + rsl.query, rsl.arg);
 			})
 			.then((res) => {
-
-				console.log('query > ', res);
-
 				resolve(res);
-				// if (!res[0])
-				// 	reject({status: "error", key: "getUsers", msg: "Nobody find !"});
-				// else
-				// 	resolve({status: "success", key: "getUsers", msg: "Find some users", data: res});
 			})
 			.catch((err) => {
-				reject({ status: "error", key: "getUsers", msg: "Query error !" });
+				reject({ status: "error", msg: "Query error !", data: [] });
 
 			})
 	})
