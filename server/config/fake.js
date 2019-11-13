a73e77c63fb552f674e1e2055c3142be026a9d1b
Lisa TRAN <ltran@e2r1p2.42.fr>
2019-11-13 18:42:25 +0100

    Add(fakProfile)

--- server/config/fake.js
+++ server/config/fake.js
@@ -1,21 +1,59 @@
 var faker = require('faker/locale/fr');
-var randomName = faker.name.findName(); // Rowan Nikolaus
-var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-var randomAddress = faker.address; // random contact card containing many properties
-var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-
 var randomProfile = require('random-profile-generator');
-var profile = randomProfile.profile();
+var randomCity = require('./cities.js');
 
-function lala() {
-
-    var obj = {}
-
-    console.log(randomCard)
-    console.log("=========")
-console.log(profile);
-
-    // console.log(faker.fake("{{name.lastName}} {{name.firstName}}, {{internet.userName}} {{internet.avatar}}, {{address.latitude}} {{address.longitude}} {{address.city}} {{address.country}}"));
+let orientation = {
+    male: [
+        { orientation: "heterosexual", identity: 17, mask: 32 },
+        { orientation: "homosexual", identity: 10, mask: 8 },
+        { orientation: "heterosexual", identity: 27, mask: 2 }
+    ],
+    female: [
+        { orientation: "heterosexual", identity: 34, mask: 16 },
+        { orientation: "homosexual", identity: 5, mask: 4 },
+        { orientation: "heterosexual", identity: 39, mask: 1 }
+    ]
 }
 
-lala()
+function getPref(gender) {
+    return (orientation[gender][Math.floor(Math.random() * 3)])
+}
+
+function FakeProfile(gender) {
+    var i = -1
+    var arr = []
+
+    var ageMin = Math.floor(Math.random() * 50) + 18
+
+    while (++i < 10) {
+
+        let geo = randomCity()
+        let male = randomProfile.profile(gender);
+        let pref = getPref(gender)
+        var obj = Object.assign({
+            username: faker.internet.userName(),
+            firstname: male.firstName,
+            lastname: male.lastName,
+            email: male.email,
+            gender: male.gender,
+            orientation: pref.orientation,
+            bio: faker.lorem.paragraph(),
+            age: male.age,
+            ageMin: ageMin,
+            ageMax: Math.floor(Math.random() * 50) + ageMin + 1,
+            distance: Math.floor(Math.random() * 276) + 25,
+            location: geo.location,
+            latitude: geo.lat,
+            longitude: geo.lng,
+            avatar: faker.internet.avatar(),
+            identity: pref.identity,
+            mask: pref.mask
+        })
+
+        arr.push(obj)
+    }
+
+    return arr
+}
+
+module.exports = { FakeProfile }
--- server/config/install.js
+++ server/config/install.js
@@ -1,5 +1,6 @@
 const mysql = require('promise-mysql');
 require('dotenv').config();
+const { FakeProfile } = require('./fake.js');
 
 async function db() {
 	try {
@@ -123,6 +124,23 @@ async function db() {
 			("Marie",  "Marie", "TRAN", "tran.lili9.lili@gmail.com", "female", "homosexual", "Je suis allergique", 1, 30, 200, 18, 25, "Paris", 48.8462217, 2.3371604999999818, "http://localhost:3300/10/kneth_pls.jpg", 1,5, 4),\
 			("Justine",  "Justine", "TRAN", "tran.lili10.lili@gmail.com", "female", "bisexual", "Mes cuisses se dechirent", 1, 26, 200, 18, 25, "Paris", 48.8331048, 2.3268889999999374, "http://localhost:3300/11/kneth_pls.jpg", 1, 39, 1);'
 		);
+
+		for (var i = 0; i < 80; i++) {
+			var rsl = (i % 2 === 1) ? FakeProfile("male") : FakeProfile("female")
+			await connection.query(
+				`INSERT INTO users (username, firstname, lastname, email, gender, orientation, bio, mailValidation, age, distance, ageMin, ageMax,location,  latitude, longitude, avatar, active, identity, mask)\
+				VALUES ("${rsl[0].username}", "${rsl[0].firstname}", "${rsl[0].lastname}", "${rsl[0].email}", "${rsl[0].gender}",  "${rsl[0].orientation}", "${rsl[0].bio}", 1, ${rsl[0].age}, ${rsl[0].distance}, ${rsl[0].ageMin}, ${rsl[0].ageMax},  "${rsl[0].location}", ${rsl[0].latitude},  ${rsl[0].longitude}, "${rsl[0].avatar}", NOW(), ${rsl[0].identity}, ${rsl[0].mask}),\
+						("${rsl[1].username}", "${rsl[1].firstname}", "${rsl[1].lastname}", "${rsl[1].email}", "${rsl[1].gender}", "${rsl[1].orientation}", "${rsl[1].bio}", 1, ${rsl[1].age}, ${rsl[1].distance}, ${rsl[1].ageMin}, ${rsl[1].ageMax},  "${rsl[1].location}", ${rsl[1].latitude},  ${rsl[1].longitude}, "${rsl[1].avatar}", NOW(), ${rsl[1].identity}, ${rsl[1].mask}),\
+						("${rsl[2].username}", "${rsl[2].firstname}", "${rsl[2].lastname}", "${rsl[2].email}", "${rsl[2].gender}", "${rsl[2].orientation}", "${rsl[2].bio}", 1, ${rsl[2].age}, ${rsl[2].distance}, ${rsl[2].ageMin}, ${rsl[2].ageMax},  "${rsl[2].location}", ${rsl[2].latitude},  ${rsl[2].longitude}, "${rsl[2].avatar}", NOW(), ${rsl[2].identity}, ${rsl[2].mask}),\
+ 						("${rsl[3].username}", "${rsl[3].firstname}", "${rsl[3].lastname}", "${rsl[3].email}", "${rsl[3].gender}", "${rsl[3].orientation}", "${rsl[3].bio}", 1, ${rsl[3].age}, ${rsl[3].distance}, ${rsl[3].ageMin}, ${rsl[3].ageMax},  "${rsl[3].location}", ${rsl[3].latitude},  ${rsl[3].longitude}, "${rsl[3].avatar}", NOW(), ${rsl[3].identity}, ${rsl[3].mask}),\
+						("${rsl[4].username}", "${rsl[4].firstname}", "${rsl[4].lastname}", "${rsl[4].email}", "${rsl[4].gender}", "${rsl[4].orientation}", "${rsl[4].bio}", 1, ${rsl[4].age}, ${rsl[4].distance}, ${rsl[4].ageMin}, ${rsl[4].ageMax},  "${rsl[4].location}", ${rsl[4].latitude},  ${rsl[4].longitude}, "${rsl[4].avatar}", NOW(), ${rsl[4].identity}, ${rsl[4].mask}),\
+						("${rsl[5].username}", "${rsl[5].firstname}", "${rsl[5].lastname}", "${rsl[5].email}", "${rsl[5].gender}", "${rsl[5].orientation}", "${rsl[5].bio}", 1, ${rsl[5].age}, ${rsl[5].distance}, ${rsl[5].ageMin}, ${rsl[5].ageMax},  "${rsl[5].location}", ${rsl[5].latitude},  ${rsl[5].longitude}, "${rsl[5].avatar}", NOW(), ${rsl[5].identity}, ${rsl[5].mask}),\
+						("${rsl[6].username}", "${rsl[6].firstname}", "${rsl[6].lastname}", "${rsl[6].email}", "${rsl[6].gender}", "${rsl[6].orientation}", "${rsl[6].bio}", 1, ${rsl[6].age}, ${rsl[6].distance}, ${rsl[6].ageMin}, ${rsl[6].ageMax},  "${rsl[6].location}", ${rsl[6].latitude},  ${rsl[6].longitude}, "${rsl[6].avatar}", NOW(), ${rsl[6].identity}, ${rsl[6].mask}),\
+						("${rsl[7].username}", "${rsl[7].firstname}", "${rsl[7].lastname}", "${rsl[7].email}", "${rsl[7].gender}", "${rsl[7].orientation}", "${rsl[7].bio}", 1, ${rsl[7].age}, ${rsl[7].distance}, ${rsl[7].ageMin}, ${rsl[7].ageMax},  "${rsl[7].location}", ${rsl[7].latitude},  ${rsl[7].longitude}, "${rsl[7].avatar}", NOW(), ${rsl[7].identity}, ${rsl[7].mask}),\
+						("${rsl[8].username}", "${rsl[8].firstname}", "${rsl[8].lastname}", "${rsl[8].email}", "${rsl[8].gender}", "${rsl[8].orientation}", "${rsl[8].bio}", 1, ${rsl[8].age}, ${rsl[8].distance}, ${rsl[8].ageMin}, ${rsl[8].ageMax},  "${rsl[8].location}", ${rsl[8].latitude},  ${rsl[8].longitude}, "${rsl[8].avatar}", NOW(), ${rsl[8].identity}, ${rsl[8].mask}),\
+						("${rsl[9].username}", "${rsl[9].firstname}", "${rsl[9].lastname}", "${rsl[9].email}", "${rsl[9].gender}", "${rsl[9].orientation}", "${rsl[9].bio}", 1, ${rsl[9].age}, ${rsl[9].distance}, ${rsl[9].ageMin}, ${rsl[9].ageMax},  "${rsl[9].location}", ${rsl[9].latitude},  ${rsl[9].longitude}, "${rsl[9].avatar}", NOW(), ${rsl[9].identity}, ${rsl[9].mask});`
+			);
+		}
 
 		await connection.end();
 	} catch (error) { console.error(error); }
