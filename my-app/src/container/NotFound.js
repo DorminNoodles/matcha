4c6758eabd2cec769815aece22e4c705e02d625a
Lisouiw <ltran@student42.fr>
2020-01-09 13:52:32 +0100

    fix(NOt found side color)

--- my-app/src/container/NotFound.js
+++ my-app/src/container/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import UserProvider from '../context/UserProvider';
+
 class NotFound extends React.Component {
 
-    render(){
-      return ( <React.Fragment>NOT FOUND</React.Fragment>);
-    }
+  static contextType = UserProvider;
+
+  UNSAFE_componentWillMount() {
+    if (this.context.header === "red-white")
+      this.context.onChange("header", "white-red")
   }
-  
-  export default NotFound ;
-  
+
+  render() {
+    return (<React.Fragment>NOT FOUND</React.Fragment>);
+  }
+}
+
+export default NotFound;
